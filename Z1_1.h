2a4a143f0dfa679bed0045c17f0e899a51acce1e
Jakub <kuba1803@ymail.com>
2022-12-10 13:38:28 +0100

    Z1_1

---
+++ Z1_1.h
@@ -0,0 +1,42 @@
+//
+// Created by Dorota on 10.12.2022.
+//
+
+#ifndef ADVENTOFCODE_Z1_1_H
+#define ADVENTOFCODE_Z1_1_H
+
+#include <iostream>
+#include <vector>
+
+class Z1_1 {
+
+public:
+    static void solve(){
+        std::string value;
+        std::vector<std::string> values;
+        while( true ){
+            std::getline(std::cin, value);
+            values.push_back(value);
+            if(value == "END"){
+                break;
+            }
+        }
+
+        int currentElves = 0;
+        int maxElves = 0;
+        for( std::string & value:values){
+            if(value.empty() || value == "END")
+                if(currentElves > maxElves){
+                    maxElves = currentElves;
+                }
+                currentElves = 0;
+                continue;
+            }
+            currentElves += stoi(value);
+        }
+        std::cout<<maxElves;
+    }
+};
+
+
+#endif //ADVENTOFCODE_Z1_1_H
